130d87f8681d05adffe03f0c7f1f8f6de8d69c09
valenxerro <s160424066@student.ubaya.ac.id>
2026-09-23 15:20:38 +0700

    finish html dashboard

--- simobile/src/app/tabs/dashboard/dashboard.page.ts
+++ simobile/src/app/tabs/dashboard/dashboard.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product';
 import { TransactionService } from '../../services/transaction';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
@@ -8,10 +9,27 @@ import { TransactionService } from '../../services/transaction';
   standalone: false,
 })
 export class DashboardPage implements OnInit {
+  // Nilai-nilai berikut dihitung oleh service lalu ditampilkan dengan interpolation binding di HTML
+  totalProduk = 0;
+  totalTransaksiHariIni = 0;
+  omzetHariIni = 0;
+  produkTerlaris = '-';
+  today = new Date();
 
   constructor(private productService: ProductService, private transactionService: TransactionService) {}
 
   ngOnInit() {
+    this.refresh();
   }
 
+  ionViewWillEnter() {
+    this.refresh();
+  }
+
+  refresh() {
+    this.totalProduk = this.productService.getAll().length;
+    this.totalTransaksiHariIni = this.transactionService.getTodayCount();
+    this.omzetHariIni = this.transactionService.getTodayTotal();
+    this.produkTerlaris = this.transactionService.getBestSellerToday();
+  }
 }
